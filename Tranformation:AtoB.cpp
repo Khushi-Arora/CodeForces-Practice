dd4e6a4238ccf5eb42f00aa696b7928b7e27b79e
Khushi-Arora <63346162+Khushi-Arora@users.noreply.github.com>
2022-01-19 01:13:00 +0530

    Update Tranformation:AtoB.cpp

--- Tranformation:AtoB.cpp
+++ Tranformation:AtoB.cpp
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include<bits/stdc++.h>
 using namespace std;
+int flag=0;
 
-void print(int n,int x,int i,int &temp[])
+void print(int n,int x,int i,vector<int> temp)
 {
+temp.push_back(x);
 if(x>n)
-return;
+{
+return ;}
 else if(x==n)
-{cout<<"Yes";
-return;}
+{cout<<"YES"<<endl;
+flag++;
+cout<<i+1<<endl;
+for(int k=0;k<=i;k++)
+{
+    cout<<temp[k]<<" ";
+}
+return ;}
 else
 {
-    cin>>temp[i];
     print(n,2*x,i+1,temp);
-    cin>>temp[i];
     print(n,10*x+1,i+1,temp);
 }
 }
 
 int main()
 {
-    int n,x,i=0,temp[100];
+    int n,x;
+    vector<int>temp;
     cin>>x>>n;
-    print(n,x,i,temp);
+    print(n,x,0,temp);
+    if(flag==0)
+    {
+        cout<<"NO";
+    }
     return 0;
 }
